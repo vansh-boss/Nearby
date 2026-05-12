5b487205288b9d822d098fa1a62aed8735618a15
Vansh Panchal <Vansh@Panchal.com>
2026-05-12 20:36:27 +0530

    fixed folder case issue

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 import { Link, useNavigate } from 'react-router-dom';
 
-import Navbar from '../../components/Navbar/Navbar';
+import Navbar from '../../Components/Navbar/Navbar';
 
 import api from '../../services/api';
 
